8adaa22ad220078ffba08befd832aeda97e8c43e
GDS_Rached <propollygds@gmail.com>
2024-02-11 19:40:13 -0800

    Clean Up Complete
    double check this crap tomorrow and write documentation / user manual

--- UI-Assets/Assets/PropollyGDS/Editor/CustomMenuItems/CreateFile/JsonToCsharpManager.cs
+++ UI-Assets/Assets/PropollyGDS/Editor/CustomMenuItems/CreateFile/JsonToCsharpManager.cs
@@ -2,41 +2,71 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using JetBrains.Annotations;
 using Unity.Plastic.Newtonsoft.Json.Linq;
 
 namespace PropollyGDS.Editor.CustomMenuItems.CreateFile
 {
-
+    /// <summary>
+    /// Represents a Data Transfer Object (DTO) for generating C# class definitions from JSON.
+    /// </summary>
     public class DTO
     {
+        /// <summary>
+        /// Gets or sets the name of the DTO.
+        /// </summary>
         public string Name { get; set; }
-        public List<Field> Fields { get; set; } = new List<Field>();
-
+        
+        /// <summary>
+        /// Gets or sets the list of fields within the DTO.
+        /// </summary>
+        public List<Field> Fields { get; set; } = new();
+        
+        /// <summary>
+        /// Returns a string representation of the DTO, including its name and fields.
+        /// </summary>
+        /// <returns>A string detailing the DTO's name and fields.</returns>
         public override string ToString()
         {
-            StringBuilder sb = new StringBuilder();
+            var sb = new StringBuilder();
             sb.AppendLine(" - " + Name);
 
             foreach (var field in Fields)
-            {
                 sb.AppendLine("     - " + field.Name + ", " + field.Type);
-            }
 
             return sb.ToString();
         }
     }
 
+    /// <summary>
+    /// Represents a single field within a DTO, including its name and type.
+    /// </summary>
     public class Field
     {
+        /// <summary>
+        /// Gets or sets the name of the field.
+        /// </summary>
         public string Name { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the type of the field.
+        /// </summary>
         public string Type { get; set; }
     }
 
+    /// <summary>
+    /// Manages the conversion of JSON structures into C# class definitions.
+    /// </summary>
     public static class JsonToCsharpManager
     {
         private static List<DTO> dtos = new();
-        private static readonly StringBuilder classDefinitions = new();
 
+        /// <summary>
+        /// Parses JSON input and generates a list of DTOs representing the C# class structure.
+        /// </summary>
+        /// <param name="json">The JSON string to parse.</param>
+        /// <param name="rootClassName">The root class name for the generated classes.</param>
+        /// <returns>A list of DTOs representing the C# classes derived from the JSON.</returns>
         public static List<DTO> GenerateClassFromJson(string json, string rootClassName = "JParsedClass")
         {
             dtos = new List<DTO>();
@@ -45,6 +75,11 @@ namespace PropollyGDS.Editor.CustomMenuItems.CreateFile
             return dtos;
         }
         
+        /// <summary>
+        /// Processes a JToken to generate DTOs based on its type.
+        /// </summary>
+        /// <param name="token">The JToken to process.</param>
+        /// <param name="className">The class name to assign to generated DTOs.</param>
         private static void ProcessToken(JToken token, string className)
         {
             switch (token.Type)
@@ -58,21 +93,30 @@ namespace PropollyGDS.Editor.CustomMenuItems.CreateFile
             }
         }
 
+        /// <summary>
+        /// Determines the appropriate C# class name for a given JSON property name.
+        /// </summary>
+        /// <param name="currentName">The JSON property name.</param>
+        /// <returns>A formatted C# class name.</returns>
         private static string GetClassName(string currentName)
         {
-            string capitalizedCurrentName = char.ToUpper(currentName[0]) + currentName.Substring(1);
-            if (currentName.StartsWith("DTO_")) return capitalizedCurrentName;
-            return $"DTO_{capitalizedCurrentName}";
+            var capitalizedCurrentName = char.ToUpper(currentName[0]) + currentName[1..];
+            return currentName.StartsWith("DTO_") ? capitalizedCurrentName : $"DTO_{capitalizedCurrentName}";
         }
 
+        /// <summary>
+        /// Processes a JObject to create or update a DTO representing a C# class.
+        /// </summary>
+        /// <param name="jObject">The JObject to process.</param>
+        /// <param name="className">The class name for the generated DTO.</param>
         private static void ProcessJObject(JObject jObject, string className)
         {
-            string objectClassName = GetClassName(className);
+            var objectClassName = GetClassName(className);
             var dto = new DTO { Name = objectClassName };
 
             foreach (var property in jObject.Properties())
             {
-                string propType = GetPropertyType(property.Value, property.Name);
+                var propType = GetPropertyType(property.Value, property.Name);
                 dto.Fields.Add(new Field { Name = property.Name, Type = propType });
             }
 
@@ -83,8 +127,14 @@ namespace PropollyGDS.Editor.CustomMenuItems.CreateFile
             }
         }
 
-        private static void ProcessJArray(JArray jArray, string className)
+        /// <summary>
+        /// Processes a JArray to generate DTOs for its elements.
+        /// </summary>
+        /// <param name="jArray">The JArray to process.</param>
+        /// <param name="className">The class name for DTOs generated from the array elements.</param>
+        private static void ProcessJArray([NotNull] JArray jArray, string className)
         {
+            if (jArray == null) throw new ArgumentNullException(nameof(jArray));
             if (jArray.Count == 0) return;
 
             var elementType = jArray.First?.Type;
@@ -94,8 +144,15 @@ namespace PropollyGDS.Editor.CustomMenuItems.CreateFile
             }
         }
 
+        /// <summary>
+        /// Attempts to determine the C# type for a given JSON property based on its value.
+        /// </summary>
+        /// <param name="token">The JToken representing the property value.</param>
+        /// <param name="propName">The name of the property.</param>
+        /// <returns>The C# type as a string.</returns>
         private static string GetPropertyType(JToken token, string propName)
         {
+            // TODO: Better system to cover more crap and get better results
             switch (token.Type)
             {
                 case JTokenType.Integer:
@@ -122,6 +179,12 @@ namespace PropollyGDS.Editor.CustomMenuItems.CreateFile
             }
         }
 
+        /// <summary>
+        /// Creates a new DTO or retrieves an existing one based on a JObject.
+        /// </summary>
+        /// <param name="jObject">The JObject to process.</param>
+        /// <param name="className">The class name for the DTO.</param>
+        /// <returns>A DTO representing the JObject.</returns>
         private static DTO CreateOrGetExistingDTO(JObject jObject, string className)
         {
             var dto = new DTO();
@@ -142,11 +205,22 @@ namespace PropollyGDS.Editor.CustomMenuItems.CreateFile
             return dto;
         }
         
+        /// <summary>
+        /// Searches for an existing DTO that matches the given new DTO.
+        /// </summary>
+        /// <param name="newDto">The DTO to find a match for.</param>
+        /// <returns>The matching DTO if found; otherwise, null.</returns>
         private static DTO FindExistingDTO(DTO newDto)
         {
             return dtos.FirstOrDefault(existingDto => IsEquivalentDTO(existingDto, newDto));
         }
 
+        /// <summary>
+        /// Checks if two DTOs are equivalent based on their name and fields.
+        /// </summary>
+        /// <param name="dto1">The first DTO to compare.</param>
+        /// <param name="dto2">The second DTO to compare.</param>
+        /// <returns>True if the DTOs are equivalent; otherwise, false.</returns>
         private static bool IsEquivalentDTO(DTO dto1, DTO dto2)
         {
             return dto1.Name == dto2.Name &&
@@ -154,29 +228,29 @@ namespace PropollyGDS.Editor.CustomMenuItems.CreateFile
                    dto1.Fields.All(f1 => dto2.Fields.Any(f2 => f1.Name == f2.Name && f1.Type == f2.Type));
         }
 
+        /// <summary>
+        /// Generates the content for a C# class based on the specified DTO.
+        /// </summary>
+        /// <param name="dto">The DTO to generate class content for.</param>
+        /// <param name="nameSpace">The namespace for the class. If null, the class will not be placed in a namespace.</param>
+        /// <returns>A string containing the C# class definition.</returns>
         public static string GenerateClassContent(DTO dto, string nameSpace = null)
         {
-            StringBuilder sb = new StringBuilder();
+            var sb = new StringBuilder();
 
-            bool usingDateTime = dto.Fields.Any(field => field.Type.StartsWith("DateTime"));
-            bool usingList = dto.Fields.Any(field => field.Type.StartsWith("List<"));
+            var usingDateTime = dto.Fields.Any(field => field.Type.StartsWith("DateTime"));
+            var usingList = dto.Fields.Any(field => field.Type.StartsWith("List<"));
             
             if (usingDateTime)
-            {
                 sb.AppendLine("using System;");
-            }
             
             if (usingList)
-            {
                 sb.AppendLine("using System.Collections.Generic;");
-            }
 
             if (usingList || usingDateTime)
-            {
                 sb.AppendLine();
-            }
             
-            string indentLevel = string.IsNullOrEmpty(nameSpace) ? "" : "    ";
+            var indentLevel = string.IsNullOrEmpty(nameSpace) ? "" : "    ";
             
             if (!string.IsNullOrEmpty(nameSpace))
             {
@@ -188,16 +262,12 @@ namespace PropollyGDS.Editor.CustomMenuItems.CreateFile
             sb.AppendLine($"{indentLevel}{{");
 
             foreach (var field in dto.Fields)
-            {
                 sb.AppendLine($"{indentLevel}    public {field.Type} {field.Name} {{ get; set; }}");
-            }
 
             sb.AppendLine($"{indentLevel}}}");
             
             if (!string.IsNullOrEmpty(nameSpace))
-            {
                 sb.AppendLine("}");
-            }
             
             return sb.ToString();
         }
--- UI-Assets/Assets/PropollyGDS/Editor/CustomMenuItems/CreateFile/TemplateManager.cs
+++ UI-Assets/Assets/PropollyGDS/Editor/CustomMenuItems/CreateFile/TemplateManager.cs
@@ -8,16 +8,25 @@ using PropollyGDS.Scripts.Extensions;
 
 namespace PropollyGDS.Editor.CustomMenuItems.CreateFile
 {
+    /// <summary>
+    /// Manages the loading and processing of script templates for file generation.
+    /// </summary>
     public class TemplateManager
     {
         private readonly Dictionary<string, Template> templates;
 
+        /// <summary>
+        /// Initializes a new instance of the TemplateManager, loading predefined templates.
+        /// </summary>
         public TemplateManager()
         {
             templates = new Dictionary<string, Template>();
             LoadTemplates();
         }
 
+        /// <summary>
+        /// Loads script templates into the manager for later use.
+        /// </summary>
         private void LoadTemplates()
         {
             templates["Abstract Class"] = new Template(Constants.ScriptingTemplates.ABSTRACT_CLASS);
@@ -29,15 +38,35 @@ namespace PropollyGDS.Editor.CustomMenuItems.CreateFile
             templates["UI Document"] = new Template(Constants.ScriptingTemplates.UI_DOCUMENT);
         }
         
-        public string[] GetTemplateKeys() => 
-            templates.Keys.ToArray();
+        /// <summary>
+        /// Retrieves all available template keys.
+        /// </summary>
+        /// <returns>An array of template keys.</returns>
+        public string[] GetTemplateKeys() => templates.Keys.ToArray();
 
-        private string GetTemplateContent(string key) => 
-            templates.TryGetValue(key, out Template template) ? template.Content : string.Empty;
+        /// <summary>
+        /// Gets the content of a template based on a provided key.
+        /// </summary>
+        /// <param name="key">The key identifying the template.</param>
+        /// <returns>The content of the template if found; otherwise, an empty string.</returns>
+        private string GetTemplateContent(string key) => templates.TryGetValue(key, out Template template) ? template.Content : string.Empty;
 
-        public Template GetTemplate(string templateType) =>
-            templates.TryGetValue(templateType, out var template) ? template : null;
+        /// <summary>
+        /// Retrieves a Template object by its type.
+        /// </summary>
+        /// <param name="templateType">The type of the template to retrieve.</param>
+        /// <returns>The Template object if found; otherwise, null.</returns>
+        public Template GetTemplate(string templateType) => templates.TryGetValue(templateType, out var template) ? template : null;
 
+        /// <summary>
+        /// Parses and processes a template, replacing placeholders and toggling sections based on provided parameters.
+        /// </summary>
+        /// <param name="directory">The directory path for namespace generation.</param>
+        /// <param name="templateKey">The key of the template to process.</param>
+        /// <param name="fileName">The name of the file to be generated.</param>
+        /// <param name="includeNamespace">Indicates whether to include a namespace in the generated content.</param>
+        /// <param name="sectionToggles">A dictionary specifying which sections to include in the generated content.</param>
+        /// <returns>The processed template content as a string.</returns>
         public string ParseAndProcessTemplate(string directory, string templateKey, string fileName, 
             bool includeNamespace, Dictionary<string, bool> sectionToggles)
         {
@@ -76,17 +105,28 @@ namespace PropollyGDS.Editor.CustomMenuItems.CreateFile
         }
     }
 
+    /// <summary>
+    /// Represents a script template, including its content and defined sections.
+    /// </summary>
     public class Template
     {
         public string Content { get; private set; }
         public IEnumerable<string> Sections { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the Template class, loading its content from a resource path.
+        /// </summary>
+        /// <param name="resourcePath">The resource path to load the template from.</param>
         public Template(string resourcePath)
         {
             LoadTemplate(resourcePath);
             ParseSections();
         }
 
+        /// <summary>
+        /// Loads the template content from a given resource path.
+        /// </summary>
+        /// <param name="resourcePath">The resource path to load the template from.</param>
         private void LoadTemplate(string resourcePath)
         {
             var textAsset = Resources.Load<TextAsset>(resourcePath);
@@ -95,6 +135,9 @@ namespace PropollyGDS.Editor.CustomMenuItems.CreateFile
             Content = textAsset.text;
         }
 
+        /// <summary>
+        /// Parses the template content to identify and store section markers.
+        /// </summary>
         private void ParseSections()
         {
             if (string.IsNullOrEmpty(Content))
